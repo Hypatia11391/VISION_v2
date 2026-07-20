16bd82f3eed0497600b19b4f505dbf9af412e1fd
SkiRanger <ski.ranger.owen@gmail.com>
2026-07-20 11:11:41 -0700

    hotfix

--- src/video_stream.cpp
+++ src/video_stream.cpp
@@ -12,18 +12,18 @@ VS::VideoStream::VideoStream(int id, VS::ThreadSafeQueue<Image>& queue)
 
 void VS::VideoStream::video_stream() {
     // Initialize camera using the persistent V4L2 path
-    cv::VideoCapture cap(Constants::cameras[0].device_path, cv::CAP_V4L2);
+    cv::VideoCapture cap(Constants::cameras[cam_id].device_path, cv::CAP_V4L2);
 
     if (!cap.isOpened()) {
-        std::cout << "Error: Could not open camera " << cam_id << " at " << device_path << std::endl;
+        std::cout << "Error: Could not open camera " << cam_id << " at " << Constants::cameras[cam_id].device_path << std::endl;
         return;
     }
 
     // Hardware configurations.
     cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G')); // <------ try YUV2. That might be bad, or might work better.
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, Constants::cameras[cam_id-1].cam_res_width);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, Constants::cameras[cam_id-1].cam_res_height);
-    cap.set(cv::CAP_PROP_FPS, Constants::cameras[cam_id-1].cam_FPS);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, Constants::cameras[cam_id].cam_res_width);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, Constants::cameras[cam_id].cam_res_height);
+    cap.set(cv::CAP_PROP_FPS, Constants::cameras[cam_id].cam_FPS);
 
     uint64_t frame_count = 0;
     cv::Mat current_frame;
